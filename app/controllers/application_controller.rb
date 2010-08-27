905c66d620cd32be6c792c7aa51a0dc421218e4b
Oleg <oleg@theworkinggroup.ca>
2010-08-27 11:57:17 -0400

    blowing away all views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery
+  
 end
--- app/controllers/cms_content_controller.rb
+++ app/controllers/cms_content_controller.rb
@@ -1,21 +1,5 @@
 class CmsContentController < ApplicationController
-  include CmsCommon::RenderPage
 
-  unloadable
-
-  def show
-    page_path = params[:path] || ""
-    @cms_page = CmsPage.published.find_by_full_path(page_path)
-
-    render_page
-  end
-
-  def sitemap
-    respond_to do |format|
-      format.xml do
-        @cms_pages = CmsPage.published
-      end
-    end
-  end
+  # TODO
 
 end
