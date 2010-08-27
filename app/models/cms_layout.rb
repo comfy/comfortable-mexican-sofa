900226d6caa7c054dfafb0b539dde541ddfc4b20
Oleg <oleg@theworkinggroup.ca>
2010-08-27 12:51:04 -0400

    blowing away more stuff

--- app/models/cms_layout.rb
+++ app/models/cms_layout.rb
@@ -1,5 +1,13 @@
 class CmsLayout < ActiveRecord::Base
   
+  # -- Relationships --------------------------------------------------------
   has_many :cms_pages, :dependent => :nullify
+  
+  # -- Validations ----------------------------------------------------------
+  validates :label,
+    :presence   => true,
+    :uniqueness => true
+  validates :content,
+    :presence   => true
   
 end
--- test/unit/cms_layout_test.rb
+++ test/unit/cms_layout_test.rb
@@ -2,8 +2,16 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class CmsLayoutTest < ActiveSupport::TestCase
   
-  def test_something
-    flunk
+  def test_fixtures_validity
+    CmsLayout.all.each do |layout|
+      assert layout.valid?
+    end
+  end
+  
+  def test_validations
+    layout = CmsLayout.create
+    assert layout.errors.present?
+    assert_has_errors_on layout, [:label, :content]
   end
   
 end
