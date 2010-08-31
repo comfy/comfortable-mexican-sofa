079037ba1ba9eebdf069c416d285561e876c457e
Hesham E <elnahhas@gmail.com>
2010-08-31 12:36:57 -0400

    moving files

--- app/controllers/cms_admin/base_controller.rb
+++ app/controllers/cms_admin/base_controller.rb
@@ -6,11 +6,12 @@ class CmsAdmin::BaseController < ApplicationController
 protected
   
   def register_asset_expansions
-    js_includes = ['jquery', 'jquery-ui', 'rails', 'cms'].collect{|f| ['cms', f].join('/')}
-    js_includes += ['tiny_mce/jquery.tinymce', 'tiny_mce/tiny_mce', 'codemirror/codemirror', 'plupload/plupload.full.min' ].collect{|f| ['cms/3rdparty', f].join('/')}
-    js_includes += ['rteditor', 'syntax_highlighter', 'uploader'].collect{|f| ['cms', f].join('/')}
-    css_includes = %w(cms_master jquery-ui).collect{|f| ['cms', f].join('/')}
+    js_includes =   [ 'jquery', 'jquery-ui', 'rails', 'cms',
+                      'tiny_mce/jquery.tinymce', 'tiny_mce/tiny_mce',
+                      'codemirror/codemirror', 'plupload/plupload.full.min',
+                      'rteditor', 'syntax_highlighter', 'uploader' ].collect{|f| ['cms', f].join('/')}
     
+    css_includes =  ['cms_master', 'jquery-ui'].collect{|f| ['cms', f].join('/')}
     
     ActionView::Helpers::AssetTagHelper.register_javascript_expansion :cms => js_includes
     ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :cms => css_includes
