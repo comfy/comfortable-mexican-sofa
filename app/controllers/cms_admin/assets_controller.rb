7fa1a1d29de45c14b27bf03e2e4b58c76c8fb9af
Hesham E <elnahhas@gmail.com>
2010-09-01 12:55:05 -0400

    Plupload: using FileUploaded response argument to handle response from server

--- app/controllers/cms_admin/assets_controller.rb
+++ app/controllers/cms_admin/assets_controller.rb
@@ -3,21 +3,15 @@ class CmsAdmin::AssetsController < CmsAdmin::BaseController
     :only => :destroy
   
   def index
-    render :update do |page|
-       page << "$('#assets_list').html(\"#{ escape_javascript(render(:partial => 'cms_admin/assets/index')) }\")"
-    end
   end
   
   def create
     @cms_asset = CmsAsset.create!(:uploaded_file => params[:file])
-    render :nothing => true
+    render(:partial => 'cms_admin/assets/asset', :object => @cms_asset)
   end
   
   def destroy
     @cms_asset.destroy
-    render :update do |page|
-      page << "$('##{dom_id(@cms_asset)}').fadeOut('slow')"
-    end
   end
 
 protected
