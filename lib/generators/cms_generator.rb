0119681c8a269e04e8b8c7549817f85d71b3160c
Oleg <oleg@theworkinggroup.ca>
2010-10-13 17:35:24 -0400

    more generator goodness

--- lib/generators/cms_generator.rb
+++ lib/generators/cms_generator.rb
@@ -9,6 +9,16 @@ class CmsGenerator < Rails::Generators::Base
     migration_template 'db/migrate/01_create_cms.rb', 'db/migrate/create_cms.rb'
   end
   
+  def generate_public_assets
+    directory 'public/stylesheets/comfortable_mexican_sofa', 'public/stylesheets/comfortable_mexican_sofa'
+    directory 'public/javascripts/comfortable_mexican_sofa', 'public/javascripts/comfortable_mexican_sofa'
+    directory 'public/images/comfortable_mexican_sofa', 'public/images/comfortable_mexican_sofa'
+  end
+  
+  def show_readme
+    readme 'lib/generators/README'
+  end
+  
   def self.next_migration_number(dirname)
     orm = Rails.configuration.generators.options[:rails][:orm]
     require "rails/generators/#{orm}"
