7a30c279ddd0258aea3c7ee4becd6b641dc7cb2e
Oleg <oleg@theworkinggroup.ca>
2011-04-30 19:20:21 -0400

    config.cache_classes = false in test env to uncover a bug I missed. need to fix failing tests somehow

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -7,8 +7,8 @@ require 'mime/types'
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
     
-    config.after_initialize do
-      Dir.glob(File.expand_path('tags/*.rb', File.dirname(__FILE__))).each do |tag_path| 
+    config.to_prepare do
+      Dir.glob(File.expand_path('tags/*.rb', File.dirname(__FILE__))).each do |tag_path|
         require tag_path
       end
     end
