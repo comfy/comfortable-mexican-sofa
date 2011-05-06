98478c4d39b47638ce962ac3b35e32eeb9c925da
Oleg <oleg@theworkinggroup.ca>
2011-05-06 15:02:50 -0400

    fixtures with serialized data actually work

--- test/unit/revisions_test.rb
+++ test/unit/revisions_test.rb
@@ -2,6 +2,22 @@ require File.expand_path('../test_helper', File.dirname(__FILE__))
 
 class RevisionsTest < ActiveSupport::TestCase
   
+  def test_fixtures_validity
+    assert_equal ({
+      'content' => 'revision content', 
+      'css'     => 'revision css',
+      'js'      => 'revision js' }), cms_revisions(:layout).data
+      
+    assert_equal ([
+      { 'label' => 'default_page_text',   'content' => 'revision page content'  },
+      { 'label' => 'default_field_text',  'content' => 'revision field content' }
+    ]), cms_revisions(:page).data
+    
+    assert_equal ({
+      'content' => 'revision content'
+    }), cms_revisions(:snippet).data
+  end
+  
   def test_init_for_layouts
     assert_equal [:content, :css, :js], cms_layouts(:default).revision_fields
   end
