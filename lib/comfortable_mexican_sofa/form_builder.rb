f82277610e5fce092082cf4108c85d8ff148ef8f
Oleg <oleg@theworkinggroup.ca>
2011-05-11 17:21:04 -0400

    fixing some styles

--- lib/comfortable_mexican_sofa/form_builder.rb
+++ lib/comfortable_mexican_sofa/form_builder.rb
@@ -56,7 +56,7 @@ class ComfortableMexicanSofa::FormBuilder < ActionView::Helpers::FormBuilder
   # -- Tag Field Fields -----------------------------------------------------
   def default_tag_field(tag, options = {})
     label     = options[:label] || tag.label.to_s.titleize
-    css_class = options[:css_class] || tag.class.name.underscore.downcase.idify
+    css_class = options[:css_class] || tag.class.to_s.demodulize.underscore
     
     field_css_class = case tag
     when ComfortableMexicanSofa::Tag::PageRichText
