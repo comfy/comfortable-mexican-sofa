4e14ee8c72af31376dfc87b09dc561607b866358
Oleg <oleg@theworkinggroup.ca>
2011-05-12 10:37:51 -0400

    wording

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -7,8 +7,8 @@ ComfortableMexicanSofa.configure do |config|
   config.authentication = 'ComfortableMexicanSofa::HttpAuth'
   
   # Default url to access admin area is http://yourhost/cms-admin/ 
-  # You can change 'cms-admin' to 'admin', for example.
-  # Set to 'nil' to disable admin entirely
+  # You can change 'cms-admin' to 'admin', for example. To disable admin area
+  # entirely set this to '' or nil
   #   config.admin_route_prefix = 'cms-admin'
   
   # By default Cms content is served directly from the root. Change this setting
