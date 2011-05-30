bcc309556f1650256246160a86f34fba561622ff
Oleg <oleg@theworkinggroup.ca>
2011-05-30 17:31:27 -0400

    cramming in default wymeditor

--- app/assets/javascripts/comfortable_mexican_sofa/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/application.js
@@ -10,7 +10,7 @@ $.CMS = function(){
     $.CMS.slugify();
     $.CMS.tree_methods();
     $.CMS.load_page_blocks();
-    // $.CMS.enable_rich_text();
+    $.CMS.enable_rich_text();
     // $.CMS.enable_codemirror();
     $.CMS.enable_date_picker();
     $.CMS.enable_sortable_list();
@@ -72,19 +72,9 @@ $.CMS = function(){
     },
 
     enable_rich_text: function(){
-      $('textarea.rich_text').tinymce({
-         theme : "advanced",
-         plugins: "",
-         theme_advanced_buttons1 : "formatselect,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,link,unlink,anchor,image,|,code",
-         theme_advanced_buttons2 : "",
-         theme_advanced_buttons3 : "",
-         theme_advanced_buttons4 : "",
-         theme_advanced_toolbar_location : "top",
-         theme_advanced_toolbar_align : "left",
-         theme_advanced_statusbar_location : "bottom",
-         theme_advanced_resizing : true,
-         theme_advanced_resize_horizontal : false
-       });
+      $('textarea.rich_text').wymeditor({
+        
+      });
     },
 
     enable_codemirror: function(){
