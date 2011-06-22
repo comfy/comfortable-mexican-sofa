ae0418c7e15d8b4e0dfda5fcc6e99f5d3a363414
Oleg <oleg@theworkinggroup.ca>
2011-06-22 17:00:57 -0400

    uploads are fine

--- app/controllers/cms_admin/uploads_controller.rb
+++ app/controllers/cms_admin/uploads_controller.rb
@@ -2,27 +2,27 @@ class CmsAdmin::UploadsController < CmsAdmin::BaseController
   
   skip_before_filter :load_fixtures
   
-  before_filter :load_cms_upload, :only => :destroy
+  before_filter :load_upload, :only => :destroy
   
   def index
     render
   end
   
   def create
-    @cms_upload = @cms_site.uploads.create!(:file => params[:file])
-    render :partial => 'file', :object => @cms_upload
+    @upload = @site.uploads.create!(:file => params[:file])
+    render :partial => 'file', :object => @upload
   rescue ActiveRecord::RecordInvalid
     render :nothing => true, :status => :bad_request
   end
   
   def destroy
-    @cms_upload.destroy
+    @upload.destroy
   end
   
 protected
   
-  def load_cms_upload
-    @cms_upload = @cms_site.uploads.find(params[:id])
+  def load_upload
+    @upload = @site.uploads.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render :nothing => true
   end
--- test/functional/cms_admin/uploads_controller_test.rb
+++ test/functional/cms_admin/uploads_controller_test.rb
@@ -4,21 +4,21 @@ class CmsAdmin::UploadsControllerTest < ActionController::TestCase
   
   def test_create
     assert_difference 'Cms::Upload.count', 1 do
-      xhr :post, :create, :file => fixture_file_upload('files/valid_image.jpg')
+      xhr :post, :create, :site_id => cms_sites(:default), :file => fixture_file_upload('files/valid_image.jpg')
       assert_response :success
     end
   end
   
   def test_create_failure
     assert_no_difference 'Cms::Upload.count' do
-      xhr :post, :create, :file => nil
+      xhr :post, :create, :site_id => cms_sites(:default), :file => nil
       assert_response :bad_request
     end
   end
   
   def test_destroy
     assert_difference 'Cms::Upload.count', -1 do
-      xhr :delete, :destroy, :id => cms_uploads(:default)
+      xhr :delete, :destroy, :site_id => cms_sites(:default), :id => cms_uploads(:default)
       assert_response :success
     end
   end
