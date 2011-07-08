88416cc9f826882bccbf75ec1bc473a432bac90d
Stephen McLeod <stephen@theworkinggroup.ca>
2011-07-08 13:11:50 -0400

    fix migration

--- db/migrate/upgrades/04_upgrade_to_1_3_0.rb
+++ db/migrate/upgrades/04_upgrade_to_1_3_0.rb
@@ -2,12 +2,12 @@ class UpgradeTo130 < ActiveRecord::Migration
   def self.up
     add_column :cms_sites, :is_mirrored, :boolean, :null => false, :default => false
     add_column :cms_sites, :path, :string
-    add_column :cms_sites, :locale, :string, :null_false, :default => 'en'
-    add_index :cms_sites, :is_mirrored
+    add_column :cms_sites, :locale, :string, :null => false, :default => 'en'
+    add_index :cms_sites, :is_mirrored 
     
-    add_column :cms_layouts,  :is_shared, :null => false, :default => false
-    add_column :cms_pages,    :is_shared, :null => false, :default => false
-    add_column :cms_snippets, :is_shared, :null => false, :default => false
+    add_column :cms_layouts,  :is_shared, :boolean, :null => false, :default => false
+    add_column :cms_pages,    :is_shared, :boolean, :null => false, :default => false
+    add_column :cms_snippets, :is_shared, :boolean, :null => false, :default => false
   end
 
   def self.down
