d5cfcac39231ab6d4cb47add4e334ad57ea21aa9
Oleg <oleg@theworkinggroup.ca>
2011-12-29 21:23:44 -0500

    attempting to fix issue with elrte and ie8

--- app/assets/javascripts/comfortable_mexican_sofa/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/application.js
@@ -96,6 +96,15 @@ $.CMS = function(){
       
       elRTE.prototype.options.toolbars.sofa = $.CMS.config.elRTE.toolbar;
       
+      // BUG: Need to set content to an empty <p> for IE
+      if ($.browser.msie){
+        $('textarea.rich_text').each(function(){
+          if ($(this).val() == ''){
+            $(this).val('<p></p>');
+          }
+        })
+      }
+      
       $('textarea.rich_text').elrte({
         height:       300,
         toolbar:      'sofa',
