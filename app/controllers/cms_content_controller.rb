18a21dd3400adef54445bfa144d25a39d561c90f
Olli Huotari <olli.huotari@iki.fi>
2012-03-18 17:11:59 +0200

    Forcing text/html content_type with render_html method.
    For some reason cms content_controller doesn't return any content type in response headers.
    
    This manifested itself after switching to rails 3.2. Normally you wont notice it but with opera browsers it suggests
    you to save the page instead of viewing the page (saving html-file instead of displaying it).
    
    Following was from firebug:
    ```
    Content-Type	; charset=utf-8
    ```

--- app/controllers/cms_content_controller.rb
+++ app/controllers/cms_content_controller.rb
@@ -14,7 +14,7 @@ class CmsContentController < ApplicationController
   def render_html(status = 200)
     if @cms_layout = @cms_page.layout
       app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
-      render :inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => :html
+      render :inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => 'text/html'
     else
       render :text => I18n.t('cms.content.layout_not_found'), :status => 404
     end
