3b4b3a85146c845cbba70dbe8b8a9fcd58894528
Oleg <oleg@khabarov.ca>
2013-01-29 16:52:24 -0500

    bye bye elRTE

--- app/assets/javascripts/comfortable_mexican_sofa/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/application.js
@@ -3,10 +3,6 @@
 //= require comfortable_mexican_sofa/jquery_ui_timepicker.js
 //= require comfortable_mexican_sofa/rails.js
 //= require comfortable_mexican_sofa/codemirror/codemirror.js
-//= require comfortable_mexican_sofa/elrte/elrte.js
-//= require comfortable_mexican_sofa/elrte/elrte.codemirror.js
-//= require comfortable_mexican_sofa/elrte/elrte.sofa_link.js
-//= require comfortable_mexican_sofa/elrte/elrte.sofa_image.js
 
 $.CMS = function(){
 
@@ -29,16 +25,7 @@ $.CMS = function(){
   });
 
   return {
-
-    // Configuration that can be overriden from the outside. For example:
-    //   $.CMS.config.elRTE.toolbar = ['undoredo']
-    config: {
-      'elRTE': {
-        toolbar: ['undoredo', 'sofa_format', 'sofa_style', 'sofa_alignment', 'lists', 'sofa_copypaste', 'sofa_image', 'sofa_links'],
-        cssfiles: []
-      }
-    },
-
+    
     enable_sortable_list: function(){
       $('.sortable, ul.sortable ul').sortable({
         handle: '.dragger',
@@ -89,29 +76,7 @@ $.CMS = function(){
     },
 
     enable_rich_text: function(){
-      elRTE.prototype.options.panels.sofa_style     = ['bold', 'italic', 'underline'];
-      elRTE.prototype.options.panels.sofa_alignment = ['justifyleft', 'justifycenter', 'justifyright'];
-      elRTE.prototype.options.panels.sofa_format    = ['formatblock'];
-      elRTE.prototype.options.panels.sofa_copypaste = ['pastetext'];
-      elRTE.prototype.options.panels.sofa_links     = ['sofa_link', 'unlink'];
-
-      elRTE.prototype.options.toolbars.sofa = $.CMS.config.elRTE.toolbar;
-      elRTE.prototype.options.cssfiles      = $.CMS.config.elRTE.cssfiles;
-      
-      // BUG: Need to set content to an empty <p> for IE
-      if ($.browser.msie){
-        $('textarea.rich_text').each(function(){
-          if ($(this).val() == ''){
-            $(this).val('<p></p>');
-          }
-        })
-      }
-
-      $('textarea.rich_text').elrte({
-        height:       300,
-        toolbar:      'sofa',
-        styleWithCSS: false
-      });
+      // TODO
     },
 
     enable_codemirror: function(){
