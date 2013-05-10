d9d3db62597055d6e5b24e752fa6af8ba757672a
David Somers <jalada@gmail.com>
2013-05-10 12:08:16 +0100

    Typo.

--- app/models/cms/user.rb
+++ app/models/cms/user.rb
@@ -11,7 +11,7 @@ class Cms::User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # -- Relationships --------------------------------------------------------
-  has_many :site_users, dependent: :destroy, class_name: 'Cms::SiteUsers'
+  has_many :site_users, dependent: :destroy, class_name: 'Cms::SiteUser'
   has_many :sites, through: :site_users
 
   # Internal: Assign sites based on token input from form.
