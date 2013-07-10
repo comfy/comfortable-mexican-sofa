a56763c156e9bff50ca104e3b17a2b0bc384a6a7
Oleg <oleg@khabarov.ca>
2013-07-10 12:45:25 -0400

    loading things

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,9 @@ Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module ComfortableMexicanSofa
   class Application < Rails::Application
+    
+    require_relative '../lib/comfortable_mexican_sofa'
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
