9821e128c2368b0058576dd42cc08e07963b71ec
Oleg <oleg@khabarov.ca>
2013-07-31 16:03:30 -0400

    allowing to override admin base controller inheritance via config

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -3,6 +3,9 @@
 ComfortableMexicanSofa.configure do |config|
   # Title of the admin area
   #   config.cms_title = 'ComfortableMexicanSofa CMS Engine'
+  
+  # Controller that is inherited from CmsAdmin::BaseController
+  #   config.base_controller = 'ApplicationController'
 
   # Module responsible for authentication. You can replace it with your own.
   # It simply needs to have #authenticate method. See http_auth.rb for reference.
