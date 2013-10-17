29278b8f2e86dd423191a7b1ef62fffbb3e312a4
Cristina Matonte <cristina.matonte@karnovgroup.com>
2013-10-17 14:49:09 +0200

    Remove old base class

--- lib/comfortable_mexican_sofa/base.rb
+++
@@ -1,9 +0,0 @@
-class ComfortableMexicanSofa::Base < ActiveRecord::Base
-  self.abstract_class = true
-
-  def self.establish_connection
-    if ComfortableMexicanSofa.config.database_config && !Rails.env.test?
-      super "#{ComfortableMexicanSofa.config.database_config}_#{Rails.env}"
-    end
-  end
-end
