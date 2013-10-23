7b99d61c8f0fb63b686ba8bf843c4f04421e8f66
Richard Kettle <richard@homeservealliance.com>
2013-10-23 14:49:58 +0100

    ton of deleted files

--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-defined?(ComfortableMexicanSofa::Application) && ComfortableMexicanSofa::Application.routes.draw do
-  ComfortableMexicanSofa::Routing.admin
-  ComfortableMexicanSofa::Routing.content :sitemap => true
-end
