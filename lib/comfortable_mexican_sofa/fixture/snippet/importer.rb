188d25f3fe19245c3f7fdd2c86c574f08888ef3e
Dylan Kendal <dkendal@freerunningtechnologies.com>
2013-11-07 13:30:03 -0800

    extracted snippet importer behavior into methods
    Change-Id: I5651673f1e5931b9232ec08a95a536811af15d65

--- lib/comfortable_mexican_sofa/fixture/snippet/importer.rb
+++ lib/comfortable_mexican_sofa/fixture/snippet/importer.rb
@@ -2,35 +2,15 @@ module ComfortableMexicanSofa::Fixture::Snippet
   class Importer < ComfortableMexicanSofa::Fixture::Importer
     def import!
       Dir["#{self.path}*/"].each do |path|
-        identifier = path.split('/').last
-        snippet = self.site.snippets.find_or_initialize_by(:identifier => identifier)
+        identifier = File.basename path
+        snippets = self.site.snippets
+        snippet = snippets.find_or_initialize_by(identifier: identifier)
 
-        # setting attributes
-        categories = []
-        if File.exists?(attrs_path = File.join(path, 'attributes.yml'))
-          if fresh_fixture?(snippet, attrs_path)
-            attrs = get_attributes(attrs_path)
+        categories = import_attributes! snippet, path
+        import_content! snippet, path
 
-            snippet.label = attrs['label']
-            categories    = attrs['categories']
-          end
-        end
-
-        # setting content
-        if File.exists?(content_path = File.join(path, 'content.html'))
-          if fresh_fixture?(snippet, content_path)
-            snippet.content = read_as_haml(content_path)
-          end
-        end
-
-        # saving
         if snippet.changed? || self.force_import
-          if snippet.save
-            save_categorizations!(snippet, categories)
-            ComfortableMexicanSofa.logger.warn("[FIXTURES] Imported Snippet \t #{snippet.identifier}")
-          else
-            ComfortableMexicanSofa.logger.warn("[FIXTURES] Failed to import Snippet \n#{snippet.errors.inspect}")
-          end
+          save_snippet snippet, categories
         end
 
         self.fixture_ids << snippet.id
@@ -39,5 +19,39 @@ module ComfortableMexicanSofa::Fixture::Snippet
       # cleaning up
       self.site.snippets.where('id NOT IN (?)', fixture_ids).each{ |s| s.destroy }
     end
+
+    private
+    def import_attributes! snippet, path
+      categories = []
+      if File.exists?(attrs_path = File.join(path, 'attributes.yml'))
+        if fresh_fixture?(snippet, attrs_path)
+          attrs = get_attributes(attrs_path)
+
+          snippet.label = attrs['label']
+          categories    = attrs['categories']
+        end
+      end
+      categories
+    end
+
+    def import_content! snippet, path
+      if File.exists?(content_path = File.join(path, 'content.html')) &&
+        fresh_fixture?(snippet, content_path)
+
+        snippet.content = read_as_haml(content_path)
+      end
+    end
+
+    def save_snippet snippet, categories
+      if snippet.save
+        save_categorizations!(snippet, categories)
+        ComfortableMexicanSofa.logger.warn(
+          "[FIXTURES] Imported Snippet \t #{snippet.identifier}")
+      else
+        ComfortableMexicanSofa.logger.warn(
+          "[FIXTURES] Failed to import Snippet \n#{snippet.errors.inspect}")
+      end
+    end
+
   end
 end
