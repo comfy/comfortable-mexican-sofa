c71c43b125430647780afb419febb1dd6fb8491a
Dave Nearing <danearing@Serenity.(none)>
2013-12-02 15:25:16 -0600

    Syntax?

--- lib/comfortable_mexican_sofa/crafty_methods.rb
+++ lib/comfortable_mexican_sofa/crafty_methods.rb
@@ -2,8 +2,9 @@ module ComfortableMexicanSofa
 	module CmsAdmin
 		module SitesController
 			def crafty_create(path,label)
-    			@site = Cms::Site.new(params[:path=>path,label => label])
+    			@site = Cms::Site.new
 	    		@site.hostname ||= request.host.downcase
+	    		@site.update_attributes(:path => path, :label => label)
 	    		@site.save!
 	    	end
 	    end
