0ab464c9f882416c23332eef12e716f3d126ea09
Oleg <oleg@khabarov.ca>
2013-12-20 14:02:58 -0500

    changing how distinct is handled

--- lib/comfortable_mexican_sofa/extensions/is_categorized.rb
+++ lib/comfortable_mexican_sofa/extensions/is_categorized.rb
@@ -22,7 +22,7 @@ module ComfortableMexicanSofa::IsCategorized
       
       scope :for_category, lambda { |*categories|
         if (categories = [categories].flatten.compact).present?
-          select("DISTINCT #{table_name}.*").
+          distinct.select("#{table_name}.*").
             joins(:categorizations => :category).
             where('cms_categories.label' => categories)
         end
