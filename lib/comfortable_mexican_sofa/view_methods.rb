45c04fa2cb2ea07c2ee80c511c44a33c802d3945
Emerson Lackey <emerson.lackey@gmail.com>
2014-02-26 17:50:41 -0500

    removing extra sanaitization

--- lib/comfortable_mexican_sofa/view_methods.rb
+++ lib/comfortable_mexican_sofa/view_methods.rb
@@ -60,11 +60,9 @@ module ComfortableMexicanSofa::ViewMethods
     end
   
     # Same as cms_block_content but with cms tags expanded
-    def cms_block_render(identifier, blockable = @cms_page, sanitize = true)
+    def cms_block_render(identifier, blockable = @cms_page)
       return '' unless tag = ComfortableMexicanSofa::ViewMethods.cms_block_tag(identifier, blockable)
-      render :inline => ComfortableMexicanSofa::Tag.process_content(
-        blockable, ComfortableMexicanSofa::Tag.sanitize_irb(tag.render, sanitize)
-      )
+      render :inline => ComfortableMexicanSofa::Tag.process_content(blockable, tag.render)
     end
   end
   
