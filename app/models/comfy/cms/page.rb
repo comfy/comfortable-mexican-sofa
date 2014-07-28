455077dcd99436d079a33a3476b8978ad8245f3c
Jonathan Steel <jonathan.steel@gshiftlabs.com>
2014-07-28 10:20:50 -0400

    Display url properly in admin panel when route path set

--- app/models/comfy/cms/page.rb
+++ app/models/comfy/cms/page.rb
@@ -68,7 +68,7 @@ class Comfy::Cms::Page < ActiveRecord::Base
   
   # Full url for a page
   def url
-    "//" + "#{self.site.hostname}/#{self.site.path}/#{self.full_path}".squeeze("/")
+    "//" + "#{self.site.hostname}/#{Rails.application.config.comfy_cms_path ? Rails.application.config.comfy_cms_path + "/" : "" }#{self.site.path}/#{self.full_path}".squeeze("/")
   end
   
 protected
--- lib/comfortable_mexican_sofa/routes/cms.rb
+++ lib/comfortable_mexican_sofa/routes/cms.rb
@@ -3,6 +3,8 @@ class ActionDispatch::Routing::Mapper
   def comfy_route_cms(options = {})
     scope :module => :comfy, :as => :comfy do
       namespace :cms, :path => options[:path] do
+        Rails.application.config.comfy_cms_path = options[:path]
+
         get 'cms-css/:site_id/:identifier(/:cache_buster)' => 'assets#render_css', :as => 'render_css'
         get 'cms-js/:site_id/:identifier(/:cache_buster)'  => 'assets#render_js',  :as => 'render_js'
 
