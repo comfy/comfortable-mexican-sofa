be0095ed6b252aa248ac5e51e50db50bfb5e5601
Jared Fraser <dev@jsf.io>
2014-09-02 12:18:45 +0100

    Added Go server scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -l
+
+set -e -x
+
+echo "no-op"
---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+
+set -e
+
+export PATH=./bin:$PATH
+export RAILS_ENV=test
+export BUNDLE_WITHOUT=development
+
+CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
+BUNDLE_JOBS=$((CORES-1))
+
+bundle install --jobs $BUNDLE_JOBS
+npm install
+
+if [ -n "$GO_PIPELINE_NAME" ]; then
+  cp config/database{-ci,}.yml
+
+  rake db:drop db:create db:schema:load db:migrate
+fi
+
+CI_PIPELINE_COUNTER=${GO_PIPELINE_COUNTER-0}
+CI_EXECUTOR_NUMBER=${EXECUTOR_NUMBER-0}
+
+rake test
