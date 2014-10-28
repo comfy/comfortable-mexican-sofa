760916399a4700de0005f2e0b4a2103dc76d7884
Fabio Napoleoni <f.napoleoni@gmail.com>
2014-10-28 16:05:59 +0100

    Memoize site variable

--- app/controllers/comfy/admin/cms/base_controller.rb
+++ app/controllers/comfy/admin/cms/base_controller.rb
@@ -21,14 +21,14 @@ class Comfy::Admin::Cms::BaseController < ComfortableMexicanSofa.config.base_con
     path = ComfortableMexicanSofa.config.admin_route_redirect
     return redirect_to(path) unless path.blank?
     load_admin_site
-    redirect_to comfy_admin_cms_site_pages_path(@site) if @site
+    redirect_to comfy_admin_cms_site_pages_path(current_site) if current_site
   end
   
 protected
   
   def load_admin_site
-    if @site = ::Comfy::Cms::Site.find_by_id(params[:site_id] || session[:site_id]) || ::Comfy::Cms::Site.first
-      session[:site_id] = @site.id
+    if current_site
+      session[:site_id] = current_site.id
     else
       I18n.locale = ComfortableMexicanSofa.config.admin_locale || I18n.default_locale
       flash[:danger] = I18n.t('comfy.admin.cms.base.site_not_found')
@@ -37,7 +37,7 @@ protected
   end
 
   def set_locale
-    I18n.locale = ComfortableMexicanSofa.config.admin_locale || (@site && @site.locale)
+    I18n.locale = ComfortableMexicanSofa.config.admin_locale || current_site.try(:locale)
     true
   end
 
@@ -45,8 +45,12 @@ protected
     return unless ComfortableMexicanSofa.config.enable_fixtures
     controllers = %w(comfy/admin/cms/layouts comfy/admin/cms/pages comfy/admin/cms/snippets)
     if controllers.member?(params[:controller])
-      ComfortableMexicanSofa::Fixture::Importer.new(@site.identifier).import!
+      ComfortableMexicanSofa::Fixture::Importer.new(current_site.identifier).import!
       flash.now[:danger] = I18n.t('comfy.admin.cms.base.fixtures_enabled')
     end
+  end
+
+  def current_site
+    @site ||= ::Comfy::Cms::Site.find_by_id(params[:site_id] || session[:site_id]) || ::Comfy::Cms::Site.first
   end
 end
