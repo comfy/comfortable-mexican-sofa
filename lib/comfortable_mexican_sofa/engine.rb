8bc5d21752e10c8dae48e70649371f474df1d8e9
Gleb Mazovetskiy <glex.spb@gmail.com>
2014-10-31 18:18:48 +0100

    Bootstrap for Sass v3.3.0 via Bower
    Enables the application to use a version of bootstrap-sass different
    from Comfy's by using Bower instead of a gem. Implements #481.

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -14,11 +14,21 @@ require 'coffee-rails'
 require 'codemirror-rails'
 require 'kaminari'
 require 'tinymce-rails'
-require 'bootstrap-sass'
 require 'plupload-rails'
 
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
-    # ...
+    # Configure asset lookup
+    initializer 'comfortable-mexican-sofa.assets' do |app|
+      # Add Bower asset paths
+      root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+        app.config.sass.load_paths << bower_path
+        app.config.assets.paths << bower_path
+      end
+      # Precompile Bootstrap fonts
+      app.config.assets.precompile << %r(comfortable_mexican_sofa/bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
+      # Minimum precision required by bootstrap-sass
+      ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+    end
   end
 end
