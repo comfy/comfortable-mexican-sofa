b7b6b867264a5dc2f4ee8773c46a51d9894607d3
Tomas D'Stefano <tomas_stefano@successoft.com>
2015-03-11 13:55:41 +0000

    Remove extra complexity for no real benefit

--- test.sh
+++ test.sh
@@ -6,10 +6,7 @@ export PATH=./bin:$PATH
 export RAILS_ENV=test
 export BUNDLE_WITHOUT=development
 
-CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-BUNDLE_JOBS=$((CORES-1))
-
-bundle install --jobs $BUNDLE_JOBS
+bundle install
 
 if [ -n "$GO_PIPELINE_NAME" ]; then
   rake db:drop db:create db:migrate
