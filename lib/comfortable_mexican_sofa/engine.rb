490c199301d4fcdb27bd5bb02df51145535d0869
Oleg <oleg@khabarov.ca>
2016-02-28 12:48:06 -0500

    easier way to override cms models/controllers

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -17,6 +17,10 @@ require 'plupload-rails'
 
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
-    # ...
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/comfortable_mexican_sofa/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
