a6a28df08b25aaf0ab58124592c125e3332dd2f3
Oleg <oleg@khabarov.ca>
2017-09-12 20:41:37 -0700

    fixing rest of integration tests

--- test/integration/routing_test.rb
+++ test/integration/routing_test.rb
@@ -1,19 +1,19 @@
 require_relative '../test_helper'
 
 class RoutingIntergrationTest < ActionDispatch::IntegrationTest
-  
+
   def teardown
     Rails.application.reload_routes!
   end
 
   def test_cms_public_prefix
-    
+
     assert_nil ComfortableMexicanSofa.config.public_cms_path
-    
+
     Rails.application.routes.draw do
-      comfy_route :cms, :path => '/custom'
+      comfy_route :cms, path: '/custom'
     end
-    
+
     assert_equal '/custom', ComfortableMexicanSofa.config.public_cms_path
   end
 end
