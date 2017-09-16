f87c205dda1e288ac7a96e4eaa1e2e131e192f6d
Oleg <oleg@khabarov.ca>
2017-09-15 19:47:41 -0700

    we can send cms_page as a context during parsing

--- lib/comfortable_mexican_sofa/tags/fragment.rb
+++ lib/comfortable_mexican_sofa/tags/fragment.rb
@@ -1,4 +1,12 @@
-# Fragment tags need the following:
+# Fragment tags
+# Examples:
+#   {% cms_fragment content %}
+#   {% cms_fragment content, namespace: left %}
+#   {% cms_fragment content, format: markdown %}
+#   {% cms_fragment meta, format: text, render: false %}
+#   {% cms_fragment header, format: file, partial: "path/to/partial", size: "200x100#"}
+#
+# Tags need the following:
 # - name - corresponds to record on the database
 # - namespace - basically a tab in the admin area. userful to group fragments
 # - render (true(default) | false) - do we render via tag, or manually access fragment content later
@@ -17,13 +25,14 @@
 class FragmentTag < Liquid::TagWithParams
 
   # Tag initialization. Need to probably validate params during init
-  def initialize(_, _, _)
+  def initialize(_, _, context)
     super
+    raise self.parse_context.inspect
   end
 
   # Rendeting out fragment content. This is where we go and grab it from the DB
-  def render(_context)
-    "hello"
+  def render(context)
+    "hello: #{context.inspect}"
   end
 end
 
--- test/lib/cms_tag_test.rb
+++ test/lib/cms_tag_test.rb
@@ -2,10 +2,16 @@ require_relative '../test_helper'
 
 class TagTest < ActiveSupport::TestCase
 
-  def test_something
-    params = 'fragment_name, type: something'
-    tag = FragmentTag.send(:new, 'cms_fragment', params, Liquid::ParseContext.new)
-    assert_equal [], tag.params
+  # def test_something
+  #   params = 'fragment_name, type: something'
+  #   tag = FragmentTag.send(:new, 'cms_fragment', params, Liquid::ParseContext.new)
+  #   assert_equal ["fragment_name", {"type"=>"something"}], tag.params
+  # end
+
+
+  def test_parsing
+    t = Liquid::Template.parse("text {% cms_fragment frag_name %} text", context: Comfy::Cms::Page.first)
+    raise t.render
   end
 
 end
