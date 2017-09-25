3e4051c483fea6f6dad7592240966e429aec9050
Oleg <oleg@khabarov.ca>
2017-09-25 15:38:32 -0700

    helper tag

---
+++ lib/comfortable_mexican_sofa/content/tags/helper.rb
@@ -0,0 +1,52 @@
+# Tag for injecting view helpers. Example tag:
+#   {{cms:helper method_name, param_a, param_b, foo: bar}}
+# This expands into something like this:
+#   <%= method_name("param_a", "param_b", "foo" => "bar") %>
+# Whitelist is can be used to control what helpers are available.
+# By default there's a blacklist of methods that should not be called.
+#
+class ComfortableMexicanSofa::Content::Tag::Helper < ComfortableMexicanSofa::Content::Tag
+
+  BLACKLIST = %w(eval class_eval instance_eval render)
+
+  attr_reader :method_name
+
+  def initialize(context, params_string)
+    super
+    @method_name = params.shift
+
+    unless @method_name.present?
+      raise Error, "Missing method name for helper tag"
+    end
+  end
+
+  # we output erb into rest of the content
+  def allow_erb
+    true
+  end
+
+  def content
+    helper_params = params.map do |p|
+      case p
+      when Hash
+        p.to_s
+      else
+        "\"#{p}\""
+      end
+    end.join(",")
+    "<%= #{method_name}(#{helper_params}) %>"
+  end
+
+  def render
+    whitelist = ComfortableMexicanSofa.config.allowed_helpers
+    if whitelist.is_a?(Array)
+      content if whitelist.map!(&:to_s).member?(method_name)
+    else
+      content unless BLACKLIST.member?(method_name)
+    end
+  end
+end
+
+ComfortableMexicanSofa::Content::Renderer.register_tag(
+  :helper, ComfortableMexicanSofa::Content::Tag::Helper
+)
---
+++ test/lib/content/tags/helper_test.rb
@@ -0,0 +1,52 @@
+require_relative '../../../test_helper'
+
+class ContentTagsHelperTest < ActiveSupport::TestCase
+
+  def test_init
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "helper_method")
+    assert_equal "helper_method", tag.method_name
+    assert_equal [], tag.params
+  end
+
+  def test_init_with_params
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "helper_method, param, key: val")
+    assert_equal "helper_method", tag.method_name
+    assert_equal ["param", {"key" => "val"}], tag.params
+  end
+
+  def test_init_without_method_name
+    message = "Missing method name for helper tag"
+    assert_exception_raised ComfortableMexicanSofa::Content::Tag::Error, message do
+      ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "")
+    end
+  end
+
+  def test_content
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "method_name, param, key: val")
+    assert_equal "<%= method_name(\"param\",{\"key\"=>\"val\"}) %>", tag.content
+  end
+
+  def test_render
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "method_name, param, key: val")
+    assert_equal "<%= method_name(\"param\",{\"key\"=>\"val\"}) %>", tag.render
+  end
+
+  def test_render_with_whitelist
+    ComfortableMexicanSofa.config.allowed_helpers = [:tester, :eval]
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "tester")
+    assert_equal "<%= tester() %>", tag.render
+
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "eval")
+    assert_equal "<%= eval() %>", tag.render
+
+    tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, "not_whitelisted")
+    assert_nil tag.render
+  end
+
+  def test_render_with_blacklist
+    ComfortableMexicanSofa::Content::Tag::Helper::BLACKLIST.each do |method|
+      tag = ComfortableMexicanSofa::Content::Tag::Helper.new(@page, method)
+      assert_nil tag.render
+    end
+  end
+end
