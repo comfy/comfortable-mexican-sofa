963c932b8ec30dfaa71676a90e058c3a36c32059
Oleg <oleg@khabarov.ca>
2017-09-26 17:57:18 -0700

    new asset link

---
+++ lib/comfortable_mexican_sofa/content/tags/asset.rb
@@ -0,0 +1,56 @@
+# This tag allows for linking of js and css content defined on the layout.
+# Looks something like this:
+#   {{cms:asset layout_identifier, type: css, as: tag}}
+#
+# `type` - css | js - what we're outputting here
+# `as`   - url (default) | tag - output url or wrap it in the appropriate tag
+#
+class ComfortableMexicanSofa::Content::Tag::Asset < ComfortableMexicanSofa::Content::Tag
+
+  attr_reader :identifier, :type, :as
+
+  def initialize(context, params_string)
+    super
+
+    options = params.extract_options!
+    @identifier = params[0]
+    @type       = options["type"]
+    @as         = options["as"] || "url"
+
+    unless @identifier.present?
+      raise Error, "Missing layout identifier for asset tag"
+    end
+  end
+
+  def layout
+    @layout ||= context.site.layouts.find_by(identifier: @identifier)
+  end
+
+  def content
+    return "" unless layout
+
+    base = ComfortableMexicanSofa.config.public_cms_path || ''
+    unless base.ends_with?("/")
+      base = base + "/"
+    end
+
+    case @type
+    when "css"
+      out = "#{base}cms-css/#{context.site.id}/#{@identifier}/#{layout.cache_buster}.css"
+      if @as == 'tag'
+        out = "<link href='#{out}' media='screen' rel='stylesheet' type='text/css' />"
+      end
+      out
+    when "js"
+      out = "#{base}cms-js/#{context.site.id}/#{@identifier}/#{layout.cache_buster}.js"
+      if @as == "tag"
+        out = "<script src='#{out}' type='text/javascript'></script>"
+      end
+      out
+    end
+  end
+end
+
+ComfortableMexicanSofa::Content::Renderer.register_tag(
+  :asset, ComfortableMexicanSofa::Content::Tag::Asset
+)
---
+++ test/lib/content/tags/asset_test.rb
@@ -0,0 +1,74 @@
+require_relative '../../../test_helper'
+
+class ContentTagsAssetTest < ActiveSupport::TestCase
+
+  setup do
+    @page = comfy_cms_pages(:default)
+  end
+
+  def test_init
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default")
+    assert_equal "default", tag.identifier
+    assert_nil tag.type
+    assert_equal "url", tag.as
+  end
+
+  def test_init_with_params
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: css, as: tag")
+    assert_equal "default", tag.identifier
+    assert_equal "css", tag.type
+    assert_equal "tag", tag.as
+  end
+
+  def test_init_without_identifier
+    message = "Missing layout identifier for asset tag"
+    assert_exception_raised ComfortableMexicanSofa::Content::Tag::Error, message do
+      ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "")
+    end
+  end
+
+  def test_layout
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default")
+    assert tag.layout.is_a?(Comfy::Cms::Layout)
+  end
+
+  def test_content_for_css
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: css")
+    out = "/cms-css/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.css"
+    assert_equal out, tag.content
+  end
+
+  def test_content_for_css_as_tag
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: css, as: tag")
+    out = "/cms-css/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.css"
+    out = "<link href='#{out}' media='screen' rel='stylesheet' type='text/css' />"
+    assert_equal out, tag.content
+  end
+
+  def test_content_for_css_with_public_cms_path
+    ComfortableMexicanSofa.config.public_cms_path = '/custom'
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: css")
+    out = "/custom/cms-css/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.css"
+    assert_equal out, tag.content
+  end
+
+  def test_content_for_js
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: js")
+    out = "/cms-js/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.js"
+    assert_equal out, tag.content
+  end
+
+  def test_content_for_js_as_tag
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: js, as: tag")
+    out = "/cms-js/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.js"
+    out = "<script src='#{out}' type='text/javascript'></script>"
+    assert_equal out, tag.content
+  end
+
+  def test_content_for_js_with_public_cms_path
+    ComfortableMexicanSofa.config.public_cms_path = '/custom'
+    tag = ComfortableMexicanSofa::Content::Tag::Asset.new(@page, "default, type: js")
+    out = "/custom/cms-js/#{@page.site_id}/#{@page.layout.identifier}/#{@page.layout.cache_buster}.js"
+    assert_equal out, tag.content
+  end
+end
