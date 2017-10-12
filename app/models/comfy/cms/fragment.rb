b8f3f929879c06af3ec5f5af59d7694db02df0ca
Oleg <oleg@khabarov.ca>
2017-10-12 13:44:17 -0700

    fragments can handle files. need to figure out how it all works with updating/removing and specifically revisions

--- app/models/comfy/cms/fragment.rb
+++ app/models/comfy/cms/fragment.rb
@@ -5,12 +5,33 @@ class Comfy::Cms::Fragment < ActiveRecord::Base
 
   serialize :content
 
-  # -- Relationships --------------------------------------------------------
+  # -- Callbacks ---------------------------------------------------------------
+  after_save :process_attachments
+
+  # -- Relationships -----------------------------------------------------------
   belongs_to :page
 
-  # -- Validations ----------------------------------------------------------
+  # -- Validations -------------------------------------------------------------
   validates :identifier,
     presence:   true,
     uniqueness: {scope: :page_id}
+
+  # -- Instance Methods --------------------------------------------------------
+
+  # Temporary storage for files as we can't attach to newly initialized Fragment
+  def content_files=(*files)
+    @content_files = files.flatten
+  end
+
+  def content_files
+    @content_files || []
+  end
+
+protected
+
+  def process_attachments
+    return if content_files.blank?
+    self.files.attach(content_files)
+  end
 
 end
--- test/models/fragment_test.rb
+++ test/models/fragment_test.rb
@@ -8,21 +8,28 @@ class CmsFragmentTest < ActiveSupport::TestCase
     @page   = comfy_cms_pages(:default)
   end
 
+  def page_params(frag_params = [])
+    {
+      layout:               @layout,
+      label:                'test page',
+      slug:                 'test_page',
+      parent_id:            @page.id,
+      fragments_attributes: frag_params
+    }
+  end
+
+  # -- Tests -------------------------------------------------------------------
+
   def test_fixtures_validity
     Comfy::Cms::Fragment.all.each do |block|
       assert block.valid?, block.errors.full_messages.to_s
     end
   end
 
-  def test_me
-    frag = Comfy::Cms::Fragment.new
-    # frag = comfy_cms_fragments(:default)
-    frag.files.attach(fixture_file_upload('files/image.jpg', 'image/jpeg'))
-
-  end
-
   def test_validation
-    flunk
+    frag = Comfy::Cms::Fragment.new
+    refute frag.valid?
+    assert_has_errors_on frag, :identifier, :page
   end
 
   def test_content_assignment
@@ -38,28 +45,42 @@ class CmsFragmentTest < ActiveSupport::TestCase
     assert_equal [1, 2, 3], fragment.content
   end
 
-  def test_content_assignment_with_files
-    fragment = Comfy::Cms::Fragment.new
+  def test_content_files
+    frag = Comfy::Cms::Fragment.new
+    assert_equal [], frag.content_files
 
-    fragment.content = fixture_file_upload('files/image.jpg', 'image/jpeg')
-    assert_nil fragment.content
-    assert_equal 1, fragment.temp_files.size
+    upload = fixture_file_upload('files/image.jpg', 'image/jpeg')
+    frag.content_files = upload
+    assert_equal [upload], frag.content_files
+  end
+
+  def test_creation
+    assert_difference "Comfy::Cms::Fragment.count" do
+      @page.fragments.create!(
+        identifier: "test",
+        content:    "test content"
+      )
+    end
+  end
+
+  def test_creation_with_files
+    frag_count        = -> {Comfy::Cms::Fragment.count}
+    attachment_count  = -> {ActiveStorage::Attachment.count}
+    assert_difference [frag_count, attachment_count] do
+      frag = @page.fragments.create!(
+        identifier:     "test",
+        content_files:  fixture_file_upload('files/image.jpg', 'image/jpeg')
+      )
+      assert frag.files.attached?
+    end
   end
 
   def test_creation_via_page_nested_attributes
     assert_difference ['Comfy::Cms::Page.count', 'Comfy::Cms::Fragment.count'] do
-      page = @site.pages.create!(
-        layout:     @layout,
-        label:      'test page',
-        slug:       'test_page',
-        parent_id:  @page.id,
-        fragments_attributes: [
-          {
-            identifier: 'default_page_text',
-            content:    'test_content'
-          }
-        ]
-      )
+      page = @site.pages.create!(page_params([{
+        identifier: 'default_page_text',
+        content:    'test_content'
+      }]))
       assert_equal 1, page.fragments.count
       fragment = page.fragments.first
       assert_equal 'default_page_text', fragment.identifier
@@ -69,18 +90,12 @@ class CmsFragmentTest < ActiveSupport::TestCase
 
   def test_creation_via_page_nested_attributes_as_hash
     assert_difference ['Comfy::Cms::Page.count', 'Comfy::Cms::Fragment.count'] do
-      page = @site.pages.create!(
-        layout:     @layout,
-        label:      'test page',
-        slug:       'test_page',
-        parent_id:  @page.id,
-        fragments_attributes: {
-          '0' => {
-            identifier: 'default_page_text',
-            content:    'test_content'
-          }
+      page = @site.pages.create!(page_params({
+        '0' => {
+          identifier: 'default_page_text',
+          content:    'test_content'
         }
-      )
+      }))
       assert_equal 1, page.fragments.count
       fragment = page.fragments.first
       assert_equal 'default_page_text', fragment.identifier
@@ -90,27 +105,28 @@ class CmsFragmentTest < ActiveSupport::TestCase
 
   def test_creation_via_page_nested_attributes_as_hash_with_duplicates
     assert_difference ['Comfy::Cms::Page.count', 'Comfy::Cms::Fragment.count'] do
-      page = @site.pages.create!(
-        layout:     @layout,
-        label:      'test page',
-        slug:       'test_page',
-        parent_id:  @page.id,
-        fragments_attributes: {
-          '0' => {
-            identifier: 'default_page_text',
-            content:    'test_content'
-          },
-          '1' => {
-            identifier: 'default_page_text',
-            content:    'test_content'
-          }
+      page = @site.pages.create!(page_params({
+        '0' => {
+          identifier: 'default_page_text',
+          content:    'test_content'
+        },
+        '1' => {
+          identifier: 'default_page_text',
+          content:    'test_content'
         }
-      )
+      }))
       assert_equal 1, page.fragments.count
       fragment = page.fragments.first
       assert_equal 'default_page_text', fragment.identifier
       assert_equal 'test_content', fragment.content
     end
+  end
+
+  def test_creation_via_nested_attributes_with_file
+    page = @site.pages.create!(page_params([{
+      identifier:   'test',
+      the_cunt: fixture_file_upload('files/image.jpg', 'image/jpeg')
+    }]))
   end
 
   def test_creation_and_update_via_nested_attributes_with_file
@@ -121,7 +137,5 @@ class CmsFragmentTest < ActiveSupport::TestCase
     flunk
   end
 
-  def test_creation_via_nested_attributes_with_file
-    flunk
-  end
+
 end
