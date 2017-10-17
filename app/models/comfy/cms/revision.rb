f6017eca9edcb372d1f182b7bbb0cf473dc6f8af
Oleg <oleg@khabarov.ca>
2017-10-16 21:52:33 -0700

    fixing revisions

--- app/models/comfy/cms/revision.rb
+++ app/models/comfy/cms/revision.rb
@@ -1,12 +1,12 @@
 class Comfy::Cms::Revision < ActiveRecord::Base
   self.table_name = 'comfy_cms_revisions'
-  
+
   serialize :data
-  
+
   # -- Relationships --------------------------------------------------------
-  belongs_to :record, :polymorphic => true
-  
+  belongs_to :record, polymorphic: true
+
   # -- Scopes ---------------------------------------------------------------
   default_scope -> { order('created_at DESC') }
-  
+
 end
