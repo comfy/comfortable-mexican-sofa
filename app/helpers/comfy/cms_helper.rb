8f7b2708bb4189f38c39968ed91a06139519e178
Oleg <oleg@khabarov.ca>
2017-11-08 12:51:27 -0800

    no more dead partial files. wrapped in a helper

--- app/helpers/comfy/cms_helper.rb
+++ app/helpers/comfy/cms_helper.rb
@@ -8,6 +8,16 @@ module Comfy
       bootstrap_form_for(record, options, &block)
     end
 
+    def comfy_admin_partial(path, params = {})
+      render path, params
+    rescue ActionView::MissingTemplate
+      if ComfortableMexicanSofa.config.reveal_cms_partials
+        content_tag(:div, class: "comfy-admin-partial") do
+          path
+        end
+      end
+    end
+
     # Injects some content somewhere inside cms admin area
     def cms_hook(name, options = {})
       ComfortableMexicanSofa::ViewHooks.render(name, self, options)
