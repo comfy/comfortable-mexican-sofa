bd613fd4bc5124161886dcff0562e4cdf7a865c2
Oleg <oleg@khabarov.ca>
2017-11-12 19:20:05 -0800

    dont need this file anymore

--- config/cms_routes.rb
+++ config/cms_routes.rb
@@ -1,6 +1,6 @@
+# We can't have this in config/routes.rb as they will get pulled in into parent
+# application automatically. We want user to manually place them.
 ComfortableMexicanSofa::Application.routes.draw do
-
   comfy_route :cms_admin
   comfy_route :cms
-
 end
