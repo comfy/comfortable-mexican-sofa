76208ccc71a2b0f740f3a44de40c6b8fa3c3468f
Oleg <oleg@khabarov.ca>
2017-11-17 11:36:01 -0800

    renaming test

--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -139,25 +139,7 @@ class CmsSiteTest < ActiveSupport::TestCase
     assert_equal site_c,  Comfy::Cms::Site.find_site("test2.host", "/fr/some/path?a=b")
   end
 
-  def test_find_site_with_site_alias
-    site_a = @site
-    site_b = Comfy::Cms::Site.create!(identifier: "site_b", hostname: "test2.host")
-
-    ComfortableMexicanSofa.config.hostname_aliases = {
-      "test.host"   => "alias_a.host",
-      "test2.host"  => %w(alias_b.host alias_c.host)
-    }
-
-    assert_equal site_a, Comfy::Cms::Site.find_site("alias_a.host")
-    assert_equal site_b, Comfy::Cms::Site.find_site("alias_b.host")
-    assert_equal site_b, Comfy::Cms::Site.find_site("alias_c.host")
-  end
-
-  def test_url
-    assert_equal "//test.host/", comfy_cms_sites(:default).url
-  end
-
-  def test_url_with_public_cms_path
+  def test_find_site_with_public_cms_path
     ComfortableMexicanSofa.config.public_cms_path = '/custom'
     assert_equal '//test.host/custom/', comfy_cms_sites(:default).url
 
@@ -176,5 +158,23 @@ class CmsSiteTest < ActiveSupport::TestCase
     assert_equal site_b,  Comfy::Cms::Site.find_site('test2.host', '/custom/fr?a=b')
     assert_equal site_b,  Comfy::Cms::Site.find_site('test2.host', '/custom/fr/some/path')
     assert_equal site_b,  Comfy::Cms::Site.find_site('test2.host', '/custom/fr/some/path?a=b')
+  end
+
+  def test_find_site_with_site_alias
+    site_a = @site
+    site_b = Comfy::Cms::Site.create!(identifier: "site_b", hostname: "test2.host")
+
+    ComfortableMexicanSofa.config.hostname_aliases = {
+      "test.host"   => "alias_a.host",
+      "test2.host"  => %w(alias_b.host alias_c.host)
+    }
+
+    assert_equal site_a, Comfy::Cms::Site.find_site("alias_a.host")
+    assert_equal site_b, Comfy::Cms::Site.find_site("alias_b.host")
+    assert_equal site_b, Comfy::Cms::Site.find_site("alias_c.host")
+  end
+
+  def test_url
+    assert_equal "//test.host/", comfy_cms_sites(:default).url
   end
 end
