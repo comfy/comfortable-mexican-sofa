dacc8c6a53ab656f8a39427948f8966b466bbd0f
Oleg <oleg@khabarov.ca>
2018-03-03 22:58:42 -0800

    unsuccessfully poking at system tests

--- test/system/files_frontend_test.rb
+++ test/system/files_frontend_test.rb
@@ -5,51 +5,42 @@ require_relative "../test_helper"
 class FilesFrontendTest < ApplicationSystemTestCase
 
   setup do
-    @site = comfy_cms_sites(:default)
+    @site     = comfy_cms_sites(:default)
+    @layout   = comfy_cms_layouts(:default)
+    @page     = comfy_cms_pages(:default)
   end
 
   def test_site_file_drag_and_drop
-    file = comfy_cms_files(:default)
-    file.attachment = ActiveStorage::Blob.create_after_upload!(
-      io: file_fixture("image.jpg").open, filename: "image.jpg", content_type: "image/jpg"
-    )
-    file.save!
-
-    comfy_cms_layouts(:default).update!(content: "{{ cms:text content }}")
-    visit_p edit_comfy_admin_cms_site_page_path(site_id: @site, id: comfy_cms_pages(:default))
-    click_link title: "Open library"
-
-    skip "Drag'n'drop from iframe to parent has issues:
-https://github.com/SeleniumHQ/selenium/issues/810"
-    drop_target = content_field
-    within_frame(:css, ".cms-files-modal iframe") do
-      find_link(file.label).drag_to(drop_target)
-    end
-    assert_equal "{{ cms:file_link #{file.id} }}", content_field.value
+    skip
+    # Dragging from iframe isn't supported by the chromedriver. This is not
+    # possible to test at the moment.
+    # See: https://github.com/SeleniumHQ/selenium/issues/810"
   end
 
   def test_page_file_drag_and_drop
-    # Upload a page file.
-    comfy_cms_layouts(:default).update!(content: <<~TEXT)
-      {{ cms:text content }}
+    @layout.update_column(:content, <<~TEXT)
       {{ cms:files attachments, render: false }}
+      {{ cms:text content }}
     TEXT
-    visit_p edit_comfy_admin_cms_site_page_path(site_id: @site, id: comfy_cms_pages(:default))
-    attach_file Rails.root.join("test/fixtures/files/image.jpg"),
-                name: "page[fragments_attributes][1][files][]", multiple: true
-    click_button I18n.t("comfy.admin.cms.pages.form.update")
-    assert_link "image.jpg"
 
-    # Dragging the link should generate CMS markup.
-    skip "Drag'n'drop does not work for some reason"
-    find_link("image.jpg").drag_to(content_field)
-    assert_equal '{{ cms:page_file_link attachments, filename: "image.jpg", as: image }}', content_field.value
-  end
+    comfy_cms_fragments(:default).update_column(:content, "")
 
-private
+    @page.update_attributes!(
+      fragments_attributes: [{
+        identifier: "attachments",
+        tag:        "files",
+        files:      fixture_file_upload("files/image.jpg", "image/jpg")
+      }]
+    )
 
-  def content_field
-    find_field(name: "page[fragments_attributes][0][content]")
+    visit_p edit_comfy_admin_cms_site_page_path(@site, @page)
+
+    text_field = find_field(name: "page[fragments_attributes][1][content]")
+    find_link("image.jpg").drag_to(text_field)
+
+    skip
+    # Figure out why dragging doesn't really happen.
+    assert_equal '{{ cms:page_file_link attachments, filename: "image.jpg", as: image }}', text_field.value
   end
 
 end
