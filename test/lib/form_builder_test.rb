908c98568db0cbc670d338ef9036d8141d42a8b4
Oleg <oleg@khabarov.ca>
2018-03-09 21:18:34 -0800

    more robust tests added. closes #801

--- test/lib/form_builder_test.rb
+++ test/lib/form_builder_test.rb
@@ -5,7 +5,7 @@ require_relative "../test_helper"
 class FormBuilderTest < ActionView::TestCase
 
   setup do
-    @page = Comfy::Cms::Page.new
+    @page = comfy_cms_pages(:default)
     @builder = ComfortableMexicanSofa::FormBuilder.new(:page, @page, self, bootstrap: { layout: "horizontal" })
   end
 
@@ -41,6 +41,22 @@ class FormBuilderTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
+  def test_fragment_field_for_text_with_content
+    tag = ComfortableMexicanSofa::Content::Tag::Text.new(context: @page, params: ["content"])
+    actual = @builder.fragment_field(tag, 123)
+    expected = <<~HTML
+      <div class="form-group row">
+        <label class="renderable-true col-form-label col-sm-2 text-sm-right" for="fragment-content">Content</label>
+        <div class="col-sm-10">
+          <input name="page[fragments_attributes][123][identifier]" type="hidden" value="content"/>
+          <input name="page[fragments_attributes][123][tag]" type="hidden" value="text"/>
+          <input class="form-control" id="fragment-content" name="page[fragments_attributes][123][content]" type="text" value="content"/>
+        </div>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
   def test_fragment_field_for_checkbox
     tag = ComfortableMexicanSofa::Content::Tag::Checkbox.new(context: @page, params: ["test"])
     actual = @builder.fragment_field(tag, 123)
@@ -53,6 +69,25 @@ class FormBuilderTest < ActionView::TestCase
           <div class="form-check mt-2">
             <input name="page[fragments_attributes][123][boolean]" type="hidden" value="0"/>
             <input class="form-check-input position-static" id="fragment-test" name="page[fragments_attributes][123][boolean]" type="checkbox" value="1"/>
+          </div>
+        </div>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_fragment_field_for_checkbox_with_value
+    tag = ComfortableMexicanSofa::Content::Tag::Checkbox.new(context: @page, params: ["boolean"])
+    actual = @builder.fragment_field(tag, 123)
+    expected = <<~HTML
+      <div class="form-group row">
+        <label class="renderable-true col-form-label col-sm-2 text-sm-right" for="fragment-boolean">Boolean</label>
+        <div class="col-sm-10">
+          <input name="page[fragments_attributes][123][identifier]" type="hidden" value="boolean"/>
+          <input name="page[fragments_attributes][123][tag]" type="hidden" value="checkbox"/>
+          <div class="form-check mt-2">
+            <input name="page[fragments_attributes][123][boolean]" type="hidden" value="0"/>
+            <input checked="checked" class="form-check-input position-static" id="fragment-boolean" name="page[fragments_attributes][123][boolean]" type="checkbox" value="1"/>
           </div>
         </div>
       </div>
@@ -109,6 +144,36 @@ class FormBuilderTest < ActionView::TestCase
     assert_xml_equal expected, actual
   end
 
+  def test_fragment_field_for_file_with_content
+    tag = ComfortableMexicanSofa::Content::Tag::File.new(context: @page, params: ["file"])
+    actual = @builder.fragment_field(tag, 123)
+
+    attachment = active_storage_attachments(:file)
+    attachment_url  = view.url_for(attachment)
+    thumb_url       = view.url_for(attachment.variant(Comfy::Cms::File::VARIANT_SIZE[:thumb]))
+
+    expected = <<~HTML
+      <div class="form-group row">
+        <label class="renderable-true col-form-label col-sm-2 text-sm-right" for="fragment-file">File</label>
+        <div class="col-sm-10">
+          <input name="page[fragments_attributes][123][identifier]" type="hidden" value="file"/>
+          <input name="page[fragments_attributes][123][tag]" type="hidden" value="file"/>
+          <input class="form-control" id="fragment-file" name="page[fragments_attributes][123][files]" type="file"/>
+          <div class="fragment-attachments">
+            <div class="fragment-attachment btn-group btn-group-sm mb-1">
+              <a class="btn btn-light text-truncate" data-cms-file-link-tag="{{ cms:page_file_link file, as: image }}" data-cms-file-thumb-url="#{thumb_url}" href="#{attachment_url}" target="_blank">fragment.jpg</a>
+              <input id="attachment_211760658" name="page[fragments_attributes][123][file_ids_destroy][]" type="checkbox" value="211760658"/>
+              <label class="btn btn-light" for="attachment_211760658">
+                <i class="fas fa-fw fa-times"/>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
   def test_fragment_field_for_files
     tag = ComfortableMexicanSofa::Content::Tag::Files.new(context: @page, params: ["test"])
     actual = @builder.fragment_field(tag, 123)
@@ -120,6 +185,36 @@ class FormBuilderTest < ActionView::TestCase
           <input name="page[fragments_attributes][123][tag]" type="hidden" value="files"/>
           <input class="form-control" id="fragment-test" multiple="multiple" name="page[fragments_attributes][123][files][]" type="file"/>
           <div class="fragment-attachments"></div>
+        </div>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_fragment_field_for_files_with_content
+    tag = ComfortableMexicanSofa::Content::Tag::Files.new(context: @page, params: ["file"])
+    actual = @builder.fragment_field(tag, 123)
+
+    attachment = active_storage_attachments(:file)
+    attachment_url  = view.url_for(attachment)
+    thumb_url       = view.url_for(attachment.variant(Comfy::Cms::File::VARIANT_SIZE[:thumb]))
+
+    expected = <<~HTML
+      <div class="form-group row">
+        <label class="renderable-true col-form-label col-sm-2 text-sm-right" for="fragment-file">File</label>
+        <div class="col-sm-10">
+          <input name="page[fragments_attributes][123][identifier]" type="hidden" value="file"/>
+          <input name="page[fragments_attributes][123][tag]" type="hidden" value="files"/>
+          <input class="form-control" id="fragment-file" multiple="multiple" name="page[fragments_attributes][123][files][]" type="file"/>
+          <div class="fragment-attachments">
+            <div class="fragment-attachment btn-group btn-group-sm mb-1">
+              <a class="btn btn-light text-truncate" data-cms-file-link-tag="{{ cms:page_file_link file, filename: &quot;fragment.jpg&quot;, as: image }}" data-cms-file-thumb-url="#{thumb_url}" href="#{attachment_url}" target="_blank">fragment.jpg</a>
+              <input id="attachment_211760658" name="page[fragments_attributes][123][file_ids_destroy][]" type="checkbox" value="211760658"/>
+              <label class="btn btn-light" for="attachment_211760658">
+                <i class="fas fa-fw fa-times"/>
+              </label>
+            </div>
+          </div>
         </div>
       </div>
     HTML
@@ -185,6 +280,22 @@ class FormBuilderTest < ActionView::TestCase
           <input name="page[fragments_attributes][123][identifier]" type="hidden" value="test"/>
           <input name="page[fragments_attributes][123][tag]" type="hidden" value="wysiwyg"/>
           <textarea data-cms-rich-text="true" id="fragment-test" name="page[fragments_attributes][123][content]"></textarea>
+        </div>
+      </div>
+    HTML
+    assert_xml_equal expected, actual
+  end
+
+  def test_form_actions
+    actual = @builder.form_actions do
+      "test"
+    end
+    expected = <<~HTML
+      <div class="form-actions row bg-light">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="form-group row mb-0">
+            <div class="col-sm-10 offset-sm-2">test</div>
+          </div>
         </div>
       </div>
     HTML
