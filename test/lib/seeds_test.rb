3d8d9dbc0293c49f424f27c1423fb05d9d7dcb52
Morgan Aldridge <morgant@makkintosshu.com>
2018-07-27 15:25:24 -0400

    Added seed tests for importing & exporting single & multiple classes. Issue #838

--- test/lib/seeds_test.rb
+++ test/lib/seeds_test.rb
@@ -26,6 +26,34 @@ class SeedsTest < ActiveSupport::TestCase
     end
   end
 
+  def test_import_single_class
+    Comfy::Cms::Page.destroy_all
+    Comfy::Cms::Layout.destroy_all
+    Comfy::Cms::Snippet.destroy_all
+
+    assert_difference(-> { Comfy::Cms::Layout.count }, 2) do
+      assert_difference(-> { Comfy::Cms::Page.count }, 0) do
+        assert_difference(-> { Comfy::Cms::Snippet.count }, 0) do
+          ComfortableMexicanSofa::Seeds::Importer.new("sample-site", "default-site").import!(["Layout"])
+        end
+      end
+    end
+  end
+
+  def test_import_multiple_classes
+    Comfy::Cms::Page.destroy_all
+    Comfy::Cms::Layout.destroy_all
+    Comfy::Cms::Snippet.destroy_all
+
+    assert_difference(-> { Comfy::Cms::Layout.count }, 2) do
+      assert_difference(-> { Comfy::Cms::Page.count }, 0) do
+        assert_difference(-> { Comfy::Cms::Snippet.count }, 1) do
+          ComfortableMexicanSofa::Seeds::Importer.new("sample-site", "default-site").import!(%w[Layout Snippet])
+        end
+      end
+    end
+  end
+
   def test_import_all_with_no_folder
     assert_exception_raised ComfortableMexicanSofa::Seeds::Error do
       ComfortableMexicanSofa::Seeds::Importer.new("invalid", "default-site").import!
@@ -48,6 +76,20 @@ class SeedsTest < ActiveSupport::TestCase
     assert_exception_raised ActiveRecord::RecordNotFound do
       ComfortableMexicanSofa::Seeds::Exporter.new("sample-site", "default-site").export!
     end
+  end
+
+  def test_export_single_class
+    host_path = File.join(ComfortableMexicanSofa.config.seeds_path, "test-site")
+    ComfortableMexicanSofa::Seeds::Exporter.new("default-site", "test-site").export!(["Layout"])
+    assert(File.exist?(File.join(host_path, "layouts")))
+    FileUtils.rm_rf(host_path)
+  end
+
+  def test_export_multiple_classes
+    host_path = File.join(ComfortableMexicanSofa.config.seeds_path, "test-site")
+    ComfortableMexicanSofa::Seeds::Exporter.new("default-site", "test-site").export!(%w[Layout Snippet])
+    assert(%w[layouts snippets].all? {|klass| File.exist?(File.join(host_path, klass))})
+    FileUtils.rm_rf(host_path)
   end
 
 end
