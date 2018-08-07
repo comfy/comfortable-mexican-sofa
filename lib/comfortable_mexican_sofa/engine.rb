d38ad4bd316159acde60446e95fdb41c93a9d2d6
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-08-07 01:08:57 +0100

    rubocop

--- lib/comfortable_mexican_sofa/engine.rb
+++ lib/comfortable_mexican_sofa/engine.rb
@@ -13,7 +13,7 @@ require "sass-rails"
 module ComfortableMexicanSofa
   class Engine < ::Rails::Engine
 
-    initializer 'comfortable_mexican_sofa.setup_assets' do
+    initializer "comfortable_mexican_sofa.setup_assets" do
       ::ComfortableMexicanSofa::Engine.config.assets.precompile += %w[
         comfy/admin/cms/application.js
         comfy/admin/cms/application.css
