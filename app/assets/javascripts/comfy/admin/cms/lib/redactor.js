2569f6f8ab56958dcf88d413a9e3e06e2b2628a8
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2019-01-15 05:31:16 +0800

    Add font-style edit fix to 1.12 (#867)

--- app/assets/javascripts/comfy/admin/cms/lib/redactor.js
+++ app/assets/javascripts/comfy/admin/cms/lib/redactor.js
@@ -7729,6 +7729,10 @@
 					{
 						var node2 = this.selection.getMarker(2);
 						this.selection.setMarker(this.range, node2, false);
+
+						// COMFY FIX
+						if (this.utils.browser('chrome')) this.caret.set(node1, 0, node2, 0);
+						// END COMFY FIX
 					}
 
 					this.savedSel = this.$editor.html();
