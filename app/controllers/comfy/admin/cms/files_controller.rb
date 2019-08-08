ab0b210786d478a2c9ad5d61bfb75e9c2ae3a32e
Niklas Bichinger <niklas@bichinger.de>
2019-08-08 22:32:13 +0200

    little create refactoring for redactor 3 (multiple file dragging)

--- app/controllers/comfy/admin/cms/files_controller.rb
+++ app/controllers/comfy/admin/cms/files_controller.rb
@@ -7,7 +7,6 @@ class Comfy::Admin::Cms::FilesController < Comfy::Admin::Cms::BaseController
 
   include ActionView::Helpers::NumberHelper
 
-  before_action :build_file,  only: %i[new create]
   before_action :load_file,   only: %i[edit update destroy]
   before_action :authorize
 
@@ -16,7 +15,7 @@ class Comfy::Admin::Cms::FilesController < Comfy::Admin::Cms::BaseController
 
     case params[:source]
 
-    # Integration with Redactor 1.0 Wysiwyg
+    # Integration with Redactor 3 Wysiwyg
     when "redactor"
       file_scope  = files_scope.limit(100).order(:position)
       file_hashes =
@@ -50,52 +49,21 @@ class Comfy::Admin::Cms::FilesController < Comfy::Admin::Cms::BaseController
   end
 
   def new
-    render
+    @file = @site.files.new(file_params)
   end
 
   def create
-    categories_scope = @site.categories.of_type("Comfy::Cms::File")
-
-    if params[:categories]
-      ids = categories_scope.where(label: params[:categories]).pluck(:id)
-      @file.category_ids = ids
-    end
-
-    # Automatically tagging upload if it's done through redactor
-    if params[:source] == "redactor"
-      category = categories_scope.find_or_create_by(label: "wysiwyg")
-      @file.category_ids = [category.id]
-    end
-
-    @file.save!
-
     case params[:source]
-    when "plupload"
-      render partial: "file", object: @file
     when "redactor"
-      render json: {
-        filelink: url_for(@file.attachment),
-        filename: @file.attachment.filename
-      }
-    else
-      flash[:success] = I18n.t("comfy.admin.cms.files.created")
-      redirect_to action: :edit, id: @file
-    end
-
-  rescue ActiveRecord::RecordInvalid
-    case params[:source]
+      _create_redactor
     when "plupload"
-      render body: @file.errors.full_messages.to_sentence, status: :unprocessable_entity
-    when "redactor"
-      render body: nil, status: :unprocessable_entity
+      _create_plupload
     else
-      flash.now[:danger] = I18n.t("comfy.admin.cms.files.creation_failure")
-      render action: :new
+      _create
     end
   end
 
   def edit
-    render
   end
 
   def update
@@ -121,8 +89,70 @@ class Comfy::Admin::Cms::FilesController < Comfy::Admin::Cms::BaseController
 
 protected
 
-  def build_file
+  def _create_redactor
+    files = @site.files.new(files_params)
+    # set category ids
+    categories_scope = @site.categories.of_type("Comfy::Cms::File")
+    category_ids = [ categories_scope.find_or_create_by(label: "wysiwyg").id ]
+    if params[:categories].present?
+      category_ids.concat( categories_scope.where(label: params[:categories]).pluck(:id) )
+    end
+
+    files.each do |file|
+      file.category_ids = category_ids
+      file.save!
+    end
+
+    result =
+        files.each_with_object({}) do |file, hash|
+          hash["file-#{file.id}"] = {
+              url: url_for(file.attachment),
+              id: file.id
+          }
+        end
+    render json: result
+  rescue ActiveRecord::RecordInvalid
+    file =
+        files&.detect do |file|
+          file.errors.any?
+        end
+    render json: {error: true, message: file && file.errors.full_messages.to_sentence}, status: :unprocessable_entity
+  end
+
+  def _create_plupload
     @file = @site.files.new(file_params)
+    # set category ids
+    categories_scope = @site.categories.of_type("Comfy::Cms::File")
+    category_ids = []
+    if params[:categories].present?
+      category_ids.concat( categories_scope.where(label: params[:categories]).pluck(:id) )
+    end
+    @file.category_ids = category_ids
+
+    @file.save!
+
+    render partial: "file", object: @file
+  rescue ActiveRecord::RecordInvalid
+    render body: @file.errors.full_messages.to_sentence, status: :unprocessable_entity
+  end
+
+  def _create
+    @file = @site.files.new(file_params)
+    # set category ids
+    categories_scope = @site.categories.of_type("Comfy::Cms::File")
+    category_ids = []
+    if params[:categories].present?
+      category_ids.concat( categories_scope.where(label: params[:categories]).pluck(:id) )
+    end
+    @file.category_ids = category_ids
+
+    @file.save!
+
+    flash[:success] = I18n.t("comfy.admin.cms.files.created")
+    redirect_to action: :edit, id: @file
+  rescue ActiveRecord::RecordInvalid
+    flash.now[:danger] = I18n.t("comfy.admin.cms.files.creation_failure")
+    render action: :new
   end
 
   def load_file
@@ -134,11 +164,17 @@ protected
 
   def file_params
     file = params[:file]
+    # new() needs a file hash to work, so create an "empty" (at least one attribute must be passed) one if no file
+    # attributes were passed.
     unless file.is_a?(Hash) || file.respond_to?(:to_unsafe_hash)
-      params[:file] = {}
-      params[:file][:file] = file
+      params[:file] = {label: nil}
     end
-    params.fetch(:file, {}).permit!
+    params.require(:file).permit(:label, :file, :description, category_ids: [])
+  end
+
+  def files_params
+    # convert `file: [<upload>, <upload2>, ...]` to `[{file: <upload>}, {file: <upload2>}, ...]`
+    params.require(:file).map! { |file| {file: file} }
   end
 
 end
